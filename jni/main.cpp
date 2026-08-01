3ae231b60cb9b4ab932b0ba7c3bc6cd72c7742f5
jdood-is-here <j.h.hamad2011@gmail.com>
2026-08-01 11:19:21 +0300

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -5,12 +5,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <pthread.h>
 #include <android/log.h>
 
 #define LOG_TAG "FruitMod"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// Function to find the base address of the game library from /proc/self/maps
 uintptr_t get_library_base(const char* library_name) {
     uintptr_t address = 0;
     char filename[32];
@@ -35,7 +35,6 @@ uintptr_t get_library_base(const char* library_name) {
 void* hack_thread(void*) {
     uintptr_t libBase = 0;
     
-    // Wait until the game library is fully loaded into memory
     while (libBase == 0) {
         libBase = get_library_base("libmortargame.so");
         if (libBase == 0) {
@@ -44,9 +43,6 @@ void* hack_thread(void*) {
     }
 
     LOGI("Successfully found libmortargame.so base address at: %p", (void*)libBase);
-
-    // Ready for memory patching or custom logic here!
-
     return NULL;
 }
 
