c1da859139f62a82c8f4a13fa757e5ff35dcc2ae
jdood-is-here <j.h.hamad2011@gmail.com>
2026-08-01 11:18:02 +0300

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,27 +1,52 @@
 #include <jni.h>
-#include <string>
 #include <unistd.h>
-#include "KittyMemory/MemoryPatch.h"
-#include "Logger.h"
+#include <sys/mman.h>
+#include <dlfcn.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <android/log.h>
 
-uintptr_t libBase = 0;
+#define LOG_TAG "FruitMod"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+// Function to find the base address of the game library from /proc/self/maps
+uintptr_t get_library_base(const char* library_name) {
+    uintptr_t address = 0;
+    char filename[32];
+    snprintf(filename, sizeof(filename), "/proc/%d/maps", getpid());
+    FILE* fp = fopen(filename, "r");
+    if (fp) {
+        char line[512];
+        while (fgets(line, sizeof(line), fp)) {
+            if (strstr(line, library_name)) {
+                uintptr_t base;
+                if (sscanf(line, "%lx-%*s", &base) == 1) {
+                    address = base;
+                    break;
+                }
+            }
+        }
+        fclose(fp);
+    }
+    return address;
+}
 
 void* hack_thread(void*) {
-    // Wait for libmortargame.so to load into memory
+    uintptr_t libBase = 0;
+    
+    // Wait until the game library is fully loaded into memory
     while (libBase == 0) {
-        libBase = KittyMemory::getLibraryBase("libmortargame.so");
+        libBase = get_library_base("libmortargame.so");
         if (libBase == 0) {
             sleep(1);
         }
     }
 
-    LOGI("libmortargame.so base address found at: %p", (void*)libBase);
+    LOGI("Successfully found libmortargame.so base address at: %p", (void*)libBase);
 
-    // Add your memory patches here using KittyMemory if needed, for example:
-    // MemoryPatch frenzyPatch = MemoryPatch::createWithHex(libBase + 0x1DAA2B0, "00 00 A0 E3");
-    // frenzyPatch.Modify();
+    // Ready for memory patching or custom logic here!
 
-    LOGI("Mod template initialized successfully!");
     return NULL;
 }
 
